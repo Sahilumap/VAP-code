db1b0bf45e2631cb5404796763ea9a99d2614d50
sahil mahendra umap <sahilumap311@gmail.com>
2026-02-06 10:07:58 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
